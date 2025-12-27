6a63b41af9e73d3d01b186e534e569842ee4c1f9
pasamjaswanthi260 <pasamjaswanthi260@gmail.com>
2025-12-27 15:02:41 +0530

    Add files via upload

---
+++ make_gif.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+
+img = Image.open("duck_actions.jpg")
+
+width, height = img.size
+
+frames = []
+frames_count = 4  
+
+frame_width = width // frames_count
+
+for i in range(frames_count):
+    left = i * frame_width
+    right = left + frame_width
+    frame = img.crop((left, 0, right, height))
+    frames.append(frame)
+
+frames[0].save(
+    "dog.gif",
+    save_all=True,
+    append_images=frames[1:],
+    duration=400,
+    loop=0
+)
+
+print("GIF created successfully")
